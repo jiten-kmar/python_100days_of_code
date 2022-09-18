63b466dd85f66a02ec170f969e9c4b495dad2971
Jitender Kumar <jitkumar0@WKMZTF546DE6.global.publicisgroupe.net>
2022-09-17 22:30:34 -0400

    list_example

--- 04_randomnu_and_lists/who_pays_the_bill.py
+++ 04_randomnu_and_lists/who_pays_the_bill.py
@@ -3,7 +3,14 @@
 import random
 payees=str(input("Enter the names of the payees sitting in the hotel: "))
 payees = payees.split(",")
-## split() function to split an input string by space. The split() method splits a string into a list.
+# ## split() function to split an input string by space. The split() method splits a string into a list.
 print(payees)
 print("Bill will be paid by",random.choice(payees))
-#Python random choice() function to select a random item from a List and Set
+# #Python random choice() function to select a random item from a List and Set
+
+## Another way is
+total_payees=len(payees)
+print(total_payees)
+random_choice=random.randint(0,total_payees - 1)
+bill_paid_by=payees[random_choice]
+print("bill to be paid by", bill_paid_by)
